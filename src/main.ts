631f3fa1bd5bc7ac770bd8baed13ebab6c2f7ff2
leadlabdev <desenvolvimento@leadlabdev.onmicrosoft.com>
2025-05-15 20:22:41 -0300

    add url encoded

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Aceita application/x-www-form-urlencoded (Twilio usa isso)
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  // Se ainda quiser aceitar JSON também:
+  app.use(bodyParser.json());
+
+  await app.listen(3000);
 }
 bootstrap();
